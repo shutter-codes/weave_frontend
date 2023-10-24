c2044a97d0cb67d2e28406c3f3d1d98a2d8e44e6
Naitik Sharma <snaitik0238@gmail.com>
2023-10-24 16:14:26 +0530

    loginPage added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import "./assets/style.css"
-import HomePage from "./pages/HomePage";
+import "./assets/style.css";
+import LoginPage from "./pages/AuthPages/LoginPageR6";
+// import HomePage from "./pages/HomePage";
+import Main from "./pages/QuickTherapy/Main";
 function App() {
-  return (  
-   <HomePage/>
-      )
-    };
+  return (
+    <div>
+     
+   <LoginPage/>
+     
+    </div>
+  );
+}
 export default App;
---
+++ src/pages/QuickTherapy/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Nav from '../../components/Nav'
+// import Hero from '../../components/Hero'
+
+function main() {
+  return (
+    <div>
+      <Nav/>
+     {/* hero srction */}
+  
+
+    </div>
+  )
+}
+
+export default main
